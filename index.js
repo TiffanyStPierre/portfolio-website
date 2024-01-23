05c589b2525140a452bdccb26359880854095740
TiffanyStPierre <stpierre.tiffany@gmail.com>
2024-01-23 09:10:50 -0700

    add collapsible project descriptions to each project

--- index.js
+++ index.js
@@ -29,4 +29,33 @@ function scrollFunction() {
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-}  
+}
+
+/*  Collapsible project descriptions  */
+
+const collapseList = document.getElementsByClassName('collapsible');
+
+for (let i = 0; i < collapseList.length; i++) {
+  collapseList[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      
+      // Navigate up to the parent div containing both the button and project details
+      const parentDiv = this.closest('.portfolio__item');
+      const content = parentDiv.querySelector('.project-details');
+
+      // Check if content is found
+      if (content) {
+          const computedStyle = window.getComputedStyle(content);
+
+          if (computedStyle.display === "none" || computedStyle.visibility === "hidden") {
+              content.style.display = "block";
+              content.style.visibility = "visible";
+          } else {
+              content.style.display = "none";
+              content.style.visibility = "hidden";
+          }
+      } else {
+          console.error("Project details not found!");
+      }
+  });
+}
